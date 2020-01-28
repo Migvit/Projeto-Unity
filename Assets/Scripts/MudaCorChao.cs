ec35f407e817429a25e801128c8926378bd53e07
Henry <henryquebagestero@gmail.com>
2020-01-28 17:23:18 -0300

    Muda Cor do Chão

---
+++ Assets/Scripts/MudaCorChao.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudaCorChao : MonoBehaviour
+{
+    private Material cor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cor = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            cor.SetColor("_BaseColor", Color.red);
+        }
+        else
+        {
+            cor.SetColor("_BaseColor", Color.green);
+        }
+    }
+}
